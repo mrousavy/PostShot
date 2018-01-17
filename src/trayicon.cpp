8bbcf9de0b3f4c83931bb047c9db0e11330fea23
Marc Rousavy <marcrousavy@hotmail.com>
2018-01-17 15:49:14 +0100

    Implement settings slot

--- src/trayicon.cpp
+++ src/trayicon.cpp
@@ -1,10 +1,15 @@
 #include "trayicon.h"
-#include <qsystemtrayicon.h>
+#include <QSystemTrayIcon>
+#include <QAction>
 
-TrayIcon::TrayIcon(QIcon& icon, QWidget* parent)
+TrayIcon::TrayIcon(QObject* parent, QIcon& icon)
+ : QObject(parent)
 {
     _trayIcon = new QSystemTrayIcon(icon, parent);
-    _menu = new QMenu("QMenu text", parent);
+    _menu = new QMenu("QMenu text");
+
+    auto* settingsAction = new QAction(tr("&Settings"));
+    QObject::connect(settingsAction, SIGNAL(QAction::triggered(bool)), this, SLOT(settings()));
 }
 
 TrayIcon::~TrayIcon()
@@ -20,4 +25,9 @@ TrayIcon::~TrayIcon()
 void TrayIcon::show()
 {
     _trayIcon->show();
+}
+
+void TrayIcon::settings()
+{
+    printf("Settings clicked.");
 }
