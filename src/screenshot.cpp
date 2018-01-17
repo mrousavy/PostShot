846d66f180363f914e0c13a39e9d65771070619f
mrousavy <mrousavy@donotreply.github.com>
2018-01-17 21:06:12 +0100

    Implement Windows Screenshot function

--- src/screenshot.cpp
+++ src/screenshot.cpp
@@ -20,4 +20,28 @@
     // Mac OS X
 #endif
 
+// OS Specific Screenshot implementations
+#ifdef Q_OS_WIN
+void Screenshot::getScreenshot(int fromX, int fromY, int toX, int toY)
+{
+    // copy screen to bitmap
+    HDC     hScreen = GetDC(NULL);
+    HDC     hDC     = CreateCompatibleDC(hScreen);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, abs(toX-fromX), abs(toY-fromY));
+    HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
+    BOOL    bRet    = BitBlt(hDC, 0, 0, abs(toX-fromX), abs(toY-fromY), hScreen, fromX, fromY, SRCCOPY);
+
+    // save bitmap to clipboard
+    OpenClipboard(NULL);
+    EmptyClipboard();
+    SetClipboardData(CF_BITMAP, hBitmap);
+    CloseClipboard();
+
+    // clean up
+    SelectObject(hDC, old_obj);
+    DeleteDC(hDC);
+    ReleaseDC(NULL, hScreen);
+    DeleteObject(hBitmap);
+}
+#elif defined(Q_OS_X11)
 #endif
