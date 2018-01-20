e0e66dcd686c015fe4b89f66bc6a02538ab88b6f
mrousavy <mrousavy@donotreply.github.com>
2018-01-20 13:34:40 +0100

    Add Screenshot functions header

--- src/screenshot.h
+++ src/screenshot.h
@@ -1,6 +1,8 @@
 #ifndef SCREENSHOT_H
 #define SCREENSHOT_H
 
+#include <QPixmap>
+
 namespace Screenshot
 {
     /*!
@@ -11,7 +13,29 @@ namespace Screenshot
      * \param toX The x coordinate to which the rectangle goes (bottom-right)
      * \param toY The y coordinate to which the rectangle goes (bottom-right)
      */
-    void getScreenshot(int fromX, int fromY, int toX, int toY);
+    QPixmap getScreenshot(int fromX, int fromY, int toX, int toY);
+
+    /*!
+     * \brief Get a Screenshot from all desktops
+     */
+    QPixmap getScreenshotFull();
+
+    /*!
+     * \brief Get a Screenshot from the Primary Screen
+     */
+    QPixmap getScreenshotPrimary();
+
+    /*!
+     * \brief Get a Screenshot from the given Window ID
+     * \param window The handle to the qt window
+     */
+    QPixmap getScreenshotWindow(const WId window);
+
+    /*!
+     * \brief Get a Screenshot from the given Qt Widget
+     * \param window The handle to the qt window
+     */
+    QPixmap getScreenshotWindow(const QWidget& widget);
 }
 
 #endif // SCREENSHOT_H
