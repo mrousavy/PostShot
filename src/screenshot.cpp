f9bcab6de969e68f2c9af5b9335e38b79acc1cb8
mrousavy <mrousavy@donotreply.github.com>
2018-01-20 13:34:52 +0100

    Implement getScreenshot with coordinates

--- src/screenshot.cpp
+++ src/screenshot.cpp
@@ -1,35 +1,17 @@
 #include "screenshot.h"
+
 #include <QtCore>
+#include <QtWidgets>
+#include <QList>
 
-// OS Specific Screenshot implementations
-#if defined(Q_OS_WIN)
 
-
-    // Windows
-    #include <Windows.h>
-    #pragma comment(lib,"user32.lib")
-    #pragma comment(lib,"Gdi32.lib")
-
-void Screenshot::getScreenshot(int fromX, int fromY, int toX, int toY)
+QPixmap Screenshot::getScreenshot(int fromX, int fromY, int toX, int toY)
 {
-    // copy screen to bitmap
-    HDC     hScreen = GetDC(NULL);
-    HDC     hDC     = CreateCompatibleDC(hScreen);
-    HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, abs(toX-fromX), abs(toY-fromY));
-    HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
-    BOOL    bRet    = BitBlt(hDC, 0, 0, abs(toX-fromX), abs(toY-fromY), hScreen, fromX, fromY, SRCCOPY);
+    QScreen *screen = QGuiApplication::primaryScreen();
+    if (!screen)
+        throw std::exception("Could not get primary screen!");
 
-    // save bitmap to clipboard
-    OpenClipboard(NULL);
-    EmptyClipboard();
-    SetClipboardData(CF_BITMAP, hBitmap);
-    CloseClipboard();
-
-    // clean up
-    SelectObject(hDC, old_obj);
-    DeleteDC(hDC);
-    ReleaseDC(NULL, hScreen);
-    DeleteObject(hBitmap);
+    return screen->grabWindow(0);
 }
 
 
