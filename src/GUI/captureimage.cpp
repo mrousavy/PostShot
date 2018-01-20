3a92791c4403e3c7b476f93e7db785e2a3eea856
mrousavy <mrousavy@donotreply.github.com>
2018-01-20 15:50:10 +0100

    Implement CaptureImage widget

--- src/GUI/captureimage.cpp
+++ src/GUI/captureimage.cpp
@@ -1,6 +1,42 @@
-#include "captureimage.h"
+#include <QApplication>
+#include <QWidget>
+#include <QScreen>
+#include <QKeyEvent>
+#include <QWindow>
+#include <functional>
 
-CaptureImage::CaptureImage(QWidget *parent) : QWidget(parent)
+#include "GUI/animation.h"
+#include "GUI/captureimage.h"
+
+CaptureImage::CaptureImage(QWidget *parent) : QWidget(parent, Qt::Window | Qt::FramelessWindowHint)
 {
+    setAttribute(Qt::WA_NoSystemBackground);
+    setWindowOpacity(0.0);
 
+    Animation::fade(this, 200, 0.0, 0.3);
+
+    auto const screen(QGuiApplication::primaryScreen());
+    setGeometry(screen->availableVirtualGeometry()); // Make fullscreen
+
+    installEventFilter(this); // Key press handler
+}
+
+
+bool CaptureImage::eventFilter(QObject* obj, QEvent* event)
+{
+    if (event->type()==QEvent::KeyPress)
+    {
+        QKeyEvent* key = static_cast<QKeyEvent*>(event);
+        if ((key->key()==Qt::Key_Escape))
+        {
+            std::function<bool(void)> func = std::bind(&CaptureImage::close, this);
+            Animation::fade(this, 200, 0.3, 0.0, &func);
+        } else
+        {
+            return QObject::eventFilter(obj, event);
+        }
+        return true;
+    }
+
+    return QObject::eventFilter(obj, event);
 }
