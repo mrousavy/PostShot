5654d37edd0e7076d874f1ad7cb3fd3d2d32049a
mrousavy <mrousavy@donotreply.github.com>
2018-01-20 16:33:57 +0100

    Resize, active and animate in CaptureImage ctor

--- src/GUI/captureimage.cpp
+++ src/GUI/captureimage.cpp
@@ -7,18 +7,22 @@
 
 #include "GUI/animation.h"
 #include "GUI/captureimage.h"
+#include "GUI/screenmanager.h"
 
 CaptureImage::CaptureImage(QWidget *parent) : QWidget(parent, Qt::Window | Qt::FramelessWindowHint)
 {
     setAttribute(Qt::WA_NoSystemBackground);
     setWindowOpacity(0.0);
 
-    Animation::fade(this, 200, 0.0, 0.3);
-
     auto const screen(QGuiApplication::primaryScreen());
     setGeometry(screen->availableVirtualGeometry()); // Make fullscreen
 
     installEventFilter(this); // Key press handler
+
+    setGeometry(ScreenManager::getVirtualDesktop()); // span across all screens
+    show();
+    activateWindow();
+    Animation::fade(this, 200, 0.0, 0.3); // fade in
 }
 
 
