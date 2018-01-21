d60af64ed49e40bfcc295efa71e0f400ce39cfaa
mrousavy <mrousavy@donotreply.github.com>
2018-01-21 10:11:43 +0100

    Implement WindowHelper to get all windows on X11/Linux

--- src/Modules/windowhelper.h
+++ src/Modules/windowhelper.h
@@ -28,5 +28,47 @@ QList<QRect> getAllWindows()
     if (!success) throw std::exception("Could not enumerate windows!");
     return windows;
 }
+
+#elif defined(Q_OS_X11)
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
+
+QList<QRect> getAllWindows()
+{
+    Atom a = XInternAtom(m_pDisplay, "_NET_CLIENT_LIST" , true);
+    Atom actualType;
+    int format;
+    unsigned long numItems, bytesAfter;
+    unsigned char *data =0;
+    int status = XGetWindowProperty(m_pDisplay,
+                                    rootWindow,
+                                    a,
+                                    0L,
+                                    (~0L),
+                                    false,
+                                    AnyPropertyType,
+                                    &actualType,
+                                    &format,
+                                    &numItems,
+                                    &bytesAfter,
+                                    &data);
+    QList<QRect> windows;
+    if (status >= Success && numItems)
+    {
+        Q_ASSERT(format == 32);
+        quint32 *array = (quint32*) data;
+        for (quint32 k = 0; k < numItems; k++)
+        {
+            Window w = (Window) array[k];
+
+            windows.append(w); // TODO: Build QRect
+        }
+        XFree(data);
+    }
+    return windows;
+}
 #endif
+
 #endif // WINDOWHELPER_H
