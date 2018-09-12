f14bbe883db5c4ef3ce3346c0b493a8d8b10919b
Marc Rousavy <marcrousavy@hotmail.com>
2018-09-12 11:22:46 +0200

    Partially fix getAllWindows

--- src/Modules/WindowHelper.cpp
+++ src/Modules/WindowHelper.cpp
@@ -1,4 +1,4 @@
-#include "Modules/WindowHelper.h"
+﻿#include "Modules/WindowHelper.h"
 
 #include <QtCore>
 #include <QDebug>
@@ -10,61 +10,61 @@ namespace WindowHelper
 
 #ifdef Q_OS_WIN
 #include <Windows.h>
-    int rw(RECT rect)
-    {
-        return rect.left - rect.right;
-    }
-    int rh(RECT rect)
-    {
-        return rect.top - rect.bottom;
-    }
+	int rw(RECT rect)
+	{
+		return rect.left - rect.right;
+	}
+	int rh(RECT rect)
+	{
+		return rect.top - rect.bottom;
+	}
 
 
-    inline BOOL CALLBACK enumWindowsProc(__in  HWND hWnd, __in  LPARAM lParam)
-    {
+	inline BOOL CALLBACK enumWindowsProc(__in  HWND hWnd, __in  LPARAM lParam)
+	{
 #ifdef IGNORE_HIDDEN
-        if (!IsWindowVisible(hWnd))
-            return TRUE;
+		if (!IsWindowVisible(hWnd))
+			return TRUE;
 #endif
-        auto tlength = GetWindowTextLength(hWnd);
+		auto tlength = GetWindowTextLength(hWnd);
 #ifdef IGNORE_NOTITLE
-        if (tlength == 0)
-            return TRUE;
+		if (tlength == 0)
+			return TRUE;
 #endif
 
-        auto windows = reinterpret_cast<QList<WindowSpec>*>(lParam);
-        RECT rect;
-        GetWindowRect(hWnd, &rect);
+		auto windows = reinterpret_cast<QList<WindowSpec>*>(lParam);
+		RECT rect;
+		GetWindowRect(hWnd, &rect);
 #ifdef IGNORE_NULLS
-        if (rw(rect) < 1 || rh(rect) < 1)
-            return TRUE;
+		if (rw(rect) < 1 || rh(rect) < 1)
+			return TRUE;
 #endif
 #ifdef IGNORE_OVERLAYS
-        QRect desktop = ScreenManager::getVirtualDesktop();
-        if (rw(rect) >= desktop.width() && rh(rect) >= desktop.height())
-            return TRUE;
+		QRect desktop = ScreenManager::getVirtualDesktop();
+		if (rw(rect) >= desktop.width() && rh(rect) >= desktop.height())
+			return TRUE;
 #endif
 
-        wchar_t title[256];
-        GetWindowText(hWnd, title, 256);
-        WindowSpec window;
-        window.rect = QRect(rect.left, rect.top, rect.right, rect.bottom);
-        window.z = currz++;
-        window.name = QString::fromWCharArray(title);
+		wchar_t title[256];
+		GetWindowText(hWnd, title, 256);
+		WindowSpec window;
+		window.rect = QRect(rect.left, rect.top, rect.right, rect.bottom);
+		window.z = currz++;
+		window.name = QString::fromWCharArray(title);
 
-        windows->append(window);
+		windows->append(window);
 
-        return TRUE;
-    }
+		return TRUE;
+	}
 
-    QList<WindowSpec> getAllWindows()
-    {
-        currz = 0;
-        QList<WindowSpec> windows;
-        BOOL success = EnumWindows(enumWindowsProc, reinterpret_cast<LPARAM>(&windows));
-        if (!success) throw std::runtime_error("Could not enumerate windows!");
-        return windows;
-    }
+	QList<WindowSpec> getAllWindows()
+	{
+		currz = 0;
+		QList<WindowSpec> windows;
+		BOOL success = EnumWindows(enumWindowsProc, reinterpret_cast<LPARAM>(&windows));
+		if (!success) throw std::runtime_error("Could not enumerate windows!");
+		return windows;
+	}
 
 #elif defined(Q_OS_LINUX)
 #include <X11/X.h>
@@ -72,38 +72,39 @@ namespace WindowHelper
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
 
-    static Display* display;
+	static Display* display;
 
-    inline QList<WindowSpec> getAllWindows()
-    {
-        if (display == nullptr) display = XOpenDisplay(NULL);
+	QList<WindowSpec> getAllWindows()
+	{
+		// TODO: getAllWindows() on linux
+		if (display == nullptr) display = XOpenDisplay(NULL);
 
-        Atom clients = XInternAtom(display, "_NET_CLIENT_LIST" , true);
-        Atom actualType;
-        int format;
-        unsigned long numItems, bytesAfter;
-        unsigned char* data = 0;
-        int status = XGetWindowProperty(display, DefaultRootWindow(display),
-                                        clients, 0L, (~0L), false, AnyPropertyType,
-                                        &actualType, &format, &numItems,
-                                        &bytesAfter, &data);
-        QList<WindowSpec> windows;
-        if (status >= Success && numItems) {
-            Q_ASSERT(format == 32);
-            quint32* array = (quint32*) data;
-            for (quint32 k = 0; k < numItems; k++) {
-                Window wId = (Window) array[k];
-                Window* root = nullptr;
-                int x, y;
-                unsigned int w, h, bw, d;
-                XGetGeometry(display, wId, root, &x, &y, &w, &h, &bw, &d);
-                WindowSpec window(QRect(x, y, w, h), 5, "TEST");
-                windows.append(window); // TODO: Build QRect
-            }
-            XFree(data);
-        }
-        return windows;
-    }
+		Atom clients = XInternAtom(display, "_NET_CLIENT_LIST" , true);
+		Atom actualType;
+		int format;
+		unsigned long numItems, bytesAfter;
+		unsigned char* data = 0;
+		int status = XGetWindowProperty(display, DefaultRootWindow(display),
+										clients, 0L, (~0L), false, AnyPropertyType,
+										&actualType, &format, &numItems,
+										&bytesAfter, &data);
+		QList<WindowSpec> windows;
+		if (status >= Success && numItems) {
+			Q_ASSERT(format == 32);
+			quint32* array = (quint32*) data;
+			for (quint32 k = 0; k < numItems; k++) {
+				Window wId = (Window) array[k];
+				Window* root = nullptr;
+				int x, y;
+				unsigned int w, h, bw, d;
+				XGetGeometry(display, wId, root, &x, &y, &w, &h, &bw, &d);
+				WindowSpec window(QRect(x, y, w, h), 5, "TEST");
+				windows.append(window); // TODO: Build QRect
+			}
+			XFree(data);
+		}
+		return windows;
+	}
 #endif
 
 }
